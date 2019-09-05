78120da1abcff262e7f54121ba53032a428a5530
Nedriia <arthur.favre@hotmail.com>
2019-09-05 12:44:09 +0200

    Cone

---
+++ Assets/CoqPointer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoqPointer : MonoBehaviour
+{
+    public Transform poule;
+    public Transform coq;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = poule.position + Vector3.up * 12;
+        transform.GetChild(0).LookAt(coq.position , Vector3.up);
+        GetComponentInChildren<TextMesh>().text = Mathf.RoundToInt((coq.position - poule.position).magnitude / 10).ToString() + "m";
+    }
+}
